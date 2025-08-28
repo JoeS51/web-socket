c63ddacd2e81dee3480b40d41b6f778c2ab31fd5
Joe Sluis <joesluis51@gmail.com>
2025-08-27 20:09:31 -0700

    updated ui

--- server/index.ts
+++ server/index.ts
@@ -33,7 +33,7 @@ io.on('connection', (socket) => {
 
   socket.on('chat', (msg) => {
     console.log(msg)
-    socket.broadcast.emit('chat', msg)
+    io.emit('chat', msg)
   })
 
   io.on('disconnect', () => {
