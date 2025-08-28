847cf42a463c722fc91a3770c482049c3c47dd29
Joe Sluis <joesluis51@gmail.com>
2025-08-27 17:41:54 -0700

    basic web socket connection between FE and BE working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import io from 'socket.io-client'
+
+const socket = io('http://localhost:3000')
+
+socket.on
 
 function App() {
   const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('Connected to server')
+    })
+
+    socket.on('chat', (msg) => {
+      console.log('Message from server:', msg)
+      messages.push(msg)
+    })
+  }, [])
+
+  const submitMessage = (e) => {
+    e.preventDefault()
+    socket.emit('chat', message)
+    setMessage('')
+  }
+
+  const messageChange = (e) => {
+    setMessage(e.target.value)
+  }
 
   return (
     <>
-      <p>test</p>
+      <div>
+        {messages.map((msg, index) => (
+          <p key={index}>{msg}</p>
+        ))}
+        <input id="input" onInput={messageChange} value={message} /><button onClick={submitMessage}>Send</button>
+      </div>
     </>
   )
 }
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,26 @@
 import express from "express";
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+const corsOptions = {
+  origin: [process.env.CLIENT_BASE_URL || 'http://localhost:3000', 'https://www.getpostman.com'],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
 const app = express();
+app.use(cors(corsOptions))
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
 
 app.use(express.json());
 
@@ -8,7 +28,20 @@ app.get("/", (req, res) => {
   res.send("hi")
 });
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+  console.log('a user has connected ts')
+
+  socket.on('chat', (msg) => {
+    console.log(msg)
+    io.emit('chat', msg)
+  })
+
+  io.on('disconnect', () => {
+    console.log("user disconnected")
+  })
+})
+
+server.listen(3000, () => {
   console.log("running server")
 })
 
